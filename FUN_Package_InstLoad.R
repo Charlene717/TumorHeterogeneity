7f138e9e59d9dfc59e15f9035e295b164a917728
CharleneMSI <q2330416@gmail.com>
2022-11-15 12:38:51 +0800

    FUN_Package_InstLoad

---
+++ FUN_Package_InstLoad.R
@@ -0,0 +1,44 @@
+## Load Packages
+#
+
+##### For few packages #####
+# if(!require("tidyverse")) install.packages("tidyverse")
+# library(tidyverse)
+
+## Set the desired organism
+# organism = "org.Hs.eg.db" ## c("org.Hs.eg.db","org.Mm.eg.db","org.Dm.eg.db")
+# c(organism,"fgsea")
+
+
+FUN_Package_InstLoad = function( Basic.set = Basic.set,
+                             BiocManager.set = BiocManager.set)
+{
+
+
+  #### Basic installation ####
+  ## Check whether the installation of those packages is required from basic
+  for (i in 1:length(Basic.set)) {
+    if (!requireNamespace(Basic.set[i], quietly = TRUE)){
+      install.packages(Basic.set[i])
+    }
+  }
+  ## Load Packages
+  lapply(Basic.set, library, character.only = TRUE)
+  rm(Basic.set,i)
+
+
+  #### BiocManager installation ####
+  ## Check whether the installation of those packages is required from BiocManager
+  if (!require("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+  BiocManager.set <- BiocManager.set
+  for (i in 1:length(BiocManager.set)) {
+    if (!requireNamespace(BiocManager.set[i], quietly = TRUE)){
+      BiocManager::install(BiocManager.set[i])
+    }
+  }
+  ## Load Packages
+  lapply(BiocManager.set, library, character.only = TRUE)
+  rm(BiocManager.set,i)
+
+}
