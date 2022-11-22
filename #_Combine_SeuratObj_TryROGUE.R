9286fb132a22071665f5a4647e55a4cf167ec910
Charlene <q2330416@gmail.com>
2022-11-22 18:28:23 +0800

    Clean up the code

--- #_Combine_SeuratObj_TryROGUE.R
+++ #_Combine_SeuratObj_TryROGUE.R
@@ -7,12 +7,10 @@
   memory.limit(300000)
 
 ##### Load Packages #####
+  #### Basic and BiocManager installation ####
   source("FUN_Package_InstLoad.R")
-  FUN_Basic.set <- c("tidyverse","Seurat","ggplot2","ggpmisc",
-                     "stringr","magrittr","dplyr")
-  FUN_BiocManager.set <- c("fgsea","AnnotationHub","ensembldb",
-                           "SeuratDisk","monocle",
-                           "SingleR","scRNAseq","celldex","scran")
+  FUN_Basic.set <- c("tidyverse","Seurat","ggpmisc","stringr","magrittr")
+  FUN_BiocManager.set <- c("ensembldb","SeuratDisk")
 
   FUN_Package_InstLoad(Basic.set = FUN_Basic.set, BiocManager.set = FUN_BiocManager.set)
   rm(FUN_Basic.set, FUN_BiocManager.set)
@@ -165,33 +163,6 @@
 #   DimPlot(scRNA.SeuObj, reduction = "umap",group.by = "Cell_type")
 #   DimPlot(scRNA.SeuObj, reduction = "umap",group.by = "DataSetID")
 #
-# ##### Cell Type Annotation #####
-#   scRNA.SeuObj@meta.data[["DataSetID"]] %>% unique()
-#   scRNA.SeuObj_Ori <- scRNA.SeuObj
-#   scRNA.SeuObj_Ref <- scRNA.SeuObj[,scRNA.SeuObj@meta.data[["DataSetID"]] %in% "PRJCA001063"]
-#   DimPlot(scRNA.SeuObj_Ref, reduction = "umap",group.by = "Cell_type")
-#
-#   RefName = "Cell_type"
-#   RefName2 = "DataSetID"
-#   Remark1 <- "PredbyCTDB"         # "PredbyscRNA_CT_GSE131886"
-#   source("CellTypeAnno_SingleR.R", encoding="UTF-8")
-#
-#   scRNA.SeuObj@meta.data[["Cell_type"]] <- scRNA.SeuObj@meta.data[[paste0("singleR_",Remark)]]
-#   scRNA.SeuObj@meta.data[["celltype"]] <- scRNA.SeuObj@meta.data[[paste0("singleR_",Remark)]]
-#   scRNA.SeuObj@meta.data[[paste0("singleR_",Remark)]] <- NULL
-#
-#   # DefaultAssay(scRNA.SeuObj) <- "RNA"
-#   FeaturePlot(scRNA.SeuObj, features = c("TOP2A"))
-#   DimPlot(scRNA.SeuObj, reduction = "umap",group.by = "Cell_type")
-#   DimPlot(scRNA.SeuObj, reduction = "umap",group.by = "ReCluster2")
-#   DimPlot(scRNA.SeuObj, reduction = "umap",group.by = "DataSetID")
-#   DimPlot(scRNA.SeuObj, reduction = "umap")
-#
-#   rm(meta_ori.df, meta_ann.df, meta.df, scRNA.SeuObj_Ori,
-#      SingleRResult.lt, CTFeatures.SeuObj)
-#   #### Save RData #####
-#   save.image(paste0(Save.Path,"/scRNA.SeuObj_CDS_PRJCA001063_Combine_Anno.RData"))
-#
 #   # #### Re-dimension reduction ####
 #   # DefaultAssay(scRNA.SeuObj) <- "integrated"
 #   # # # scRNA.SeuObj <- FindVariableFeatures(scRNA.SeuObj, selection.method = "vst", nfeatures = 2000)
@@ -208,76 +179,6 @@
 #
 #   #### Save RData #####
 #   save.image(paste0(Save.Path,"/scRNA.SeuObj_CDS_PRJCA001063_Combine_Anno_ReDR.RData"))
-#
-# ##### Export figures #####
-#   ## Export TIFF
-#   for (i in seq(50,400,50)) {
-#     for (j in seq(0.1,0.7,0.2)) {
-#       for (k in seq(20,300,40)) {
-#         try({
-#           set.seed(1)
-#           scRNA.SeuObj <- RunUMAP(scRNA.SeuObj, dims = 1:i,n.neighbors = k, min.dist= j)
-#           scRNA.SeuObj@meta.data[[paste0("UMAP_PCA",i,"_NNe",k,"_MD03",j)]] <- scRNA.SeuObj@reductions[["umap"]]@cell.embeddings
-#           # scRNA.SeuObj@reductions[["umap"]]@cell.embeddings <- scRNA.SeuObj@meta.data[[paste0("UMAP_PCA",i,"_NNe",k,"_MD03",j)]]
-#
-#           Idents(scRNA.SeuObj) <- scRNA.SeuObj@meta.data[["Cell_type"]]
-#           p <-  DimPlot(scRNA.SeuObj, reduction = "umap", label = TRUE, pt.size = 0.5) + NoLegend() %>% BeautifyggPlot(LegPos = c(1.02, 0.5)) +
-#             ggtitle(paste0("CellType","  PCA:",i,"  NNe:",k,"  MD:",j)) +
-#             theme(plot.title = element_text(hjust = 0.5,vjust = 0))
-#           tiff(file = paste0(Save.Path,"/",ProjectName,"_Trajectory","_PCA",i,"_NNe",k,"_MD",j,"_CellType.tiff"),
-#                width = 28, height = 20, units = "cm", res = 200)
-#           print(p)
-#           graphics.off()
-#
-#           Idents(scRNA.SeuObj) <- scRNA.SeuObj@meta.data[["ReCluster2"]]
-#           p <-  DimPlot(scRNA.SeuObj, reduction = "umap", label = TRUE, pt.size = 0.5) + NoLegend() %>% BeautifyggPlot(LegPos = c(1.02, 0.5)) +
-#             ggtitle(paste0("ReCluster2","  PCA:",i,"  NNe:",k,"  MD:",j)) +
-#             theme(plot.title = element_text(hjust = 0.5,vjust = 0))
-#           tiff(file = paste0(Save.Path,"/",ProjectName,"_Trajectory","_PCA",i,"_NNe",k,"_MD",j,"_ReCluster.tiff"),
-#                width = 35, height = 20, units = "cm", res = 200)
-#           print(p)
-#           graphics.off()
-#
-#           Idents(scRNA.SeuObj) <- scRNA.SeuObj@meta.data[["DataSetID"]]
-#           p <-  DimPlot(scRNA.SeuObj, reduction = "umap", label = TRUE, pt.size = 0.5) + NoLegend() %>% BeautifyggPlot(LegPos = c(1.02, 0.5)) +
-#             ggtitle(paste0("ReCluster","  PCA:",i,"  NNe:",k,"  MD:",j)) +
-#             theme(plot.title = element_text(hjust = 0.5,vjust = 0))
-#           tiff(file = paste0(Save.Path,"/",ProjectName,"_Trajectory","_PCA",i,"_NNe",k,"_MD",j,"_DataSetID.tiff"),
-#                width = 35, height = 20, units = "cm", res = 200)
-#           print(p)
-#           graphics.off()
-#
-#           p <-  FeaturePlot(scRNA.SeuObj, features = c("TOP2A")) %>% BeautifyggPlot(LegPos = c(1.02, 0.15)) +
-#             ggtitle(paste0("TOP2A","  PCA:",i,"  NNe:",k,"  MD:",j)) +
-#             theme(plot.title = element_text(hjust = 0.5,vjust = 0))
-#           tiff(file = paste0(Save.Path,"/",ProjectName,"_Trajectory","_PCA",i,"_NNe",k,"_MD",j,"_TOP2A.tiff"),
-#                width = 28, height = 20, units = "cm", res = 200)
-#           print(p)
-#           graphics.off()
-#         })
-#       }
-#     }
-#   }
-#
-#
-#
-#   # scRNA.SeuObj <- RunUMAP(scRNA.SeuObj, dims = 1:100,n.neighbors = 20, min.dist=0.05)
-#   # # scRNA.SeuObj <- RunUMAP(scRNA.SeuObj, dims = 1:100,n.neighbors = 1000, min.dist=0.1)
-#   # # scRNA.SeuObj@meta.data[["UMAP_NNe1000"]] <- scRNA.SeuObj@reductions[["umap"]]@cell.embeddings
-#   # # scRNA.SeuObj@meta.data <- scRNA.SeuObj@meta.data[,!colnames(scRNA.SeuObj@meta.data)=="UMAP_NNe1000"]
-#   # scRNA.SeuObj@meta.data[["UMAP_NNe20_MD03"]] <- scRNA.SeuObj@reductions[["umap"]]@cell.embeddings
-#   #
-#   # DimPlot(scRNA.SeuObj, reduction = "umap", label = TRUE, pt.size = 0.5) + NoLegend()
-#
-#   Idents(scRNA.SeuObj) <- scRNA.SeuObj@meta.data[["Cell_type"]]
-#   DimPlot(scRNA.SeuObj, reduction = "umap", label = TRUE, pt.size = 0.5) + NoLegend()
-#
-#   Idents(scRNA.SeuObj) <- scRNA.SeuObj@meta.data[["ReCluster"]]
-#   DimPlot(scRNA.SeuObj, reduction = "umap", label = TRUE, pt.size = 0.5) + NoLegend()
-#   FeaturePlot(scRNA.SeuObj, features = c("TOP2A"))
-#
-#   #### Save RData #####
-#   save.image(paste0(Save.Path,"/scRNA.SeuObj_CDS_PRJCA001063_Combine_TryCondition.RData"))
 #
 #
 #   ##### Session information #####
