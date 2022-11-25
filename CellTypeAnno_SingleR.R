63344d073b7020e543a2ff48460c0a99cafa014c
Charlene <q2330416@gmail.com>
2022-11-26 05:59:16 +0800

    Clean up the code

--- CellTypeAnno_SingleR.R
+++ CellTypeAnno_SingleR.R
@@ -35,159 +35,234 @@ CTFeatures.SeuObj <- scRNA.SeuObj_Ref
 # scRNA.SeuObj_Ref
 
 
+##### Current path and new folder setting* #####
+ProjectName = paste0("CTAnno_singleR_RefPRJCA001063")
+Sampletype = "PDAC"
+#ProjSamp.Path = paste0(Sampletype,"_",ProjectName)
+
+Version = paste0(Sys.Date(),"_",ProjectName,"_",Sampletype)
+Save.Path = paste0(getwd(),"/",Version)
+## Create new folder
+if (!dir.exists(Save.Path)){
+  dir.create(Save.Path)
+}
+
 ##### Parameter setting* #####
-# Remark1 <- "PredbyscRNA" # c("PredbyCTDB","PredbyscRNA")
+Remark = "PredbyscRNA" # c("PredbyCTDB","PredbyscRNA")
 RefType <- "BuiltIn_scRNA" # c("BuiltIn_celldex","BuiltIn_scRNA")
 celldexDatabase <- "HumanPrimaryCellAtlasData"
 # c("BlueprintEncodeData","DatabaseImmuneCellExpressionData","HumanPrimaryCellAtlasData","ImmGenData",
 #   "MonacoImmuneData","MouseRNAseqData","NovershternHematopoieticData")
-de.method <- "classic"
-
-## Parameter of classifySingleR
-quantile = 0.8
-tune.thresh = 0.05
-sd.thresh = 1
-
-Remark <- paste0(Remark1,"_",de.method,"_",
-                 "qua",quantile,"_tun",tune.thresh,"_sd",sd.thresh)
-
-SmallTest = F
+SingleR_DE_method <- "classic"
 
 
+##### Set References #####
+if(RefType == "BuiltIn_celldex"){
+  #### Database: Bulk reference setting for Cell type features ####
+  library(celldex)
 
-#####  #####
-if(SmallTest == TRUE){
-  ## SeuObj_Ref for small test
-  # CTFeatures.SeuObj <- scRNA.SeuObj_Ref[,scRNA.SeuObj_Ref$CELL %in% sample(scRNA.SeuObj_Ref$CELL,1000)] ## For small test
-  CTFeatures.SeuObj <- scRNA.SeuObj_Ref[,scRNA.SeuObj_Ref@meta.data[[1]] %in% sample(scRNA.SeuObj_Ref@meta.data[[1]],1000)] ## For small test
-  ## SeuObj_Tar for small test
-  # scRNA.SeuObj <- scRNA.SeuObj[,scRNA.SeuObj$CELL %in% sample(scRNA.SeuObj$CELL,1000)] ## For small test
-  scRNA.SeuObj <- scRNA.SeuObj[,scRNA.SeuObj@meta.data[[1]] %in% sample(scRNA.SeuObj@meta.data[[1]],1000)] ## For small test
-}else{
-  ## SeuObj_Ref for full data
-  CTFeatures.SeuObj <- scRNA.SeuObj_Ref
+  if(celldexDatabase == "BlueprintEncodeData"){
+    CTFeatures <- BlueprintEncodeData()
+  }else if(celldexDatabase == "DatabaseImmuneCellExpressionData"){
+    CTFeatures <- DatabaseImmuneCellExpressionData()
+  }else if(celldexDatabase == "HumanPrimaryCellAtlasData"){
+    CTFeatures <- HumanPrimaryCellAtlasData()
+  }else if(celldexDatabase == "ImmGenData"){
+    CTFeatures <- ImmGenData()
+  }else if(celldexDatabase == "MonacoImmuneData"){
+    CTFeatures <- MonacoImmuneData()
+  }else if(celldexDatabase == "MouseRNAseqData"){
+    CTFeatures <- MouseRNAseqData()
+  }else if(celldexDatabase == "NovershternHematopoieticData"){
+    CTFeatures <- NovershternHematopoieticData()
+  }else{
+    print("Error in database setting!")
+  }
+
+
+  #### Demo dataset ####
+  # library(celldex)
+  # hpca.se <- HumanPrimaryCellAtlasData()
+  # hpca.se
+}else if(RefType =="BuiltIn_scRNA"){
+  #### single-cell reference setting for Cell type features ####
+  ## Prepossessing
+  CTFeatures <- as.SingleCellExperiment(CTFeatures.SeuObj)
+  CTFeatures$label <- CTFeatures@colData@listData[["Cell_type"]]
+  CTFeatures <- CTFeatures[,!is.na(CTFeatures$label)]
+  # CTFeatures <- logNormCounts(CTFeatures)
+  #rm(CTFeatures.SeuObj)
+
+  #### Demo dataset ####
+  # library(scRNAseq)
+  # sceM <- MuraroPancreasData()
+  #
+  # # One should normally do cell-based quality control at this point, but for
+  # # brevity's sake, we will just remove the unlabelled libraries here.
+  # sceM <- sceM[,!is.na(sceM$label)]
+  #
+  # # SingleR() expects reference datasets to be normalized and log-transformed.
+  # library(scuttle)
+  # sceM <- logNormCounts(sceM)
+
 }
 
+# #########################################################################################################
+# ##### Combine scRNA-seq and Bulk as Ref #####
+# CTFeatures_Ori <- CTFeatures
+#
+# ##### Filter feature #####
+# CTFeatures_Human <- HumanPrimaryCellAtlasData()
+# CTFeatures_Human@colData@listData[["label.main"]] %>% unique()
+# CTFeatures_Human_S <- CTFeatures_Human[,CTFeatures_Human@colData@listData[["label.main"]] %in%
+#                                          c("Endothelial_cells","Epithelial_cells","Fibroblasts",
+#                                            "Macrophage","T_cells","B_cell","Neuroepithelial_cell",
+#                                            "NK_cell","DC","Monocyte")]
+# CTFeatures_Human_S2 <- CTFeatures_Human[,CTFeatures_Human@colData@listData[["label.main"]] %in%
+#                                          c("HSC_-G-CSF","HSC_CD34+","Myelocyte",
+#                                            "Pre-B_cell_CD34-","Pro-B_cell_CD34+","Pro-Myelocyte",
+#                                            "GMP")]
+#
+# CTFeatures_Human_sc <- as(CTFeatures_Human,"SingleCellExperiment")
+# CTFeatures_Ori2 <- as(CTFeatures_Ori,"SummarizedExperiment")
+# CTFeatures_Ori2 <- CTFeatures_Ori2[,1:ncol(CTFeatures_Human)]
+# CTFeatures_Human_TTT <-  CTFeatures_Human[1:nrow(CTFeatures_Ori2),]
+# # CTFeatures_Human_TTT <-  CTFeatures_Human[,1:ncol(CTFeatures_Ori2)]
+# # colnames(CTFeatures_Human_TTT) <- colnames(CTFeatures_Ori2)
+# CTFeatures <- cbind(CTFeatures_Human_TTT,CTFeatures_Ori2)
+
+
+#########################################################################################################
 
 
 
-##### Current path and new folder setting* #####
-  ProjectName_Ano = paste0("CTAnno_singleR_PRJCA001063S")
-  Sampletype = "PDAC"
+##### Set Target SeuObj #####
+## Prepossessing
+scRNA <- as.SingleCellExperiment(scRNA.SeuObj)
+#### Demo dataset ####
+# library(scRNAseq)
+# hESCs <- LaMannoBrainData('human-es')
+# hESCs <- hESCs[,colSums(counts(hESCs)) > 0] # Remove libraries with no counts.
+# hESCs <- logNormCounts(hESCs)
+# hESCs <- hESCs[,1:100]
 
-  # Version = paste0(Sys.Date(),"_",ProjectName_Ano,"_",Sampletype)
-  # Save.Path = paste0(getwd(),"/",Version)
-  # ## Create new folder
-  # if (!dir.exists(Save.Path)){
-  #   dir.create(Save.Path)
-  # }
+#### Run SingleR ####
+library(SingleR)
+if(RefType == "BuiltIn_celldex"){
+  SingleR.lt <- SingleR(test = scRNA, ref = CTFeatures, assay.type.test=1,
+                        labels = CTFeatures$label.main , de.method= SingleR_DE_method)#, de.method="wilcox") #  de.method = c("classic", "wilcox", "t")
+
+}else if(RefType =="BuiltIn_scRNA"){
+  SingleR.lt <- SingleR(test = scRNA, ref = CTFeatures, assay.type.test=1,
+                        labels = CTFeatures$label , de.method= SingleR_DE_method)#, de.method="wilcox") #  de.method = c("classic", "wilcox", "t")
+}
+
+SingleR.lt
+
+# Summarizing the distribution:
+CTCount_byCTDB.df <- table(SingleR.lt$labels) %>%
+  as.data.frame() %>%
+  dplyr::rename(Cell_Type = Var1, Count = Freq)
+
+##### Annotation diagnostics #####
+p.ScoreHeatmap1 <- plotScoreHeatmap(SingleR.lt)
+p.ScoreHeatmap1
+p.DeltaDist1 <- plotDeltaDistribution(SingleR.lt, ncol = 3)
+p.DeltaDist1
+summary(is.na(SingleR.lt$pruned.labels))
+
+pdf(file = paste0(Save.Path,"/",ProjectName,"_",Remark,"_AnnoDiag.pdf"),
+    width = 10,  height = 7
+)
+p.ScoreHeatmap1 %>% print()
+p.DeltaDist1 %>% print()
+dev.off()
+
+all.markers <- metadata(SingleR.lt)$de.genes
+scRNA@colData@listData[[paste0("labels_",SingleR_DE_method,"_",Remark)]] <- SingleR.lt$labels ## scRNA$labels <- SingleR.lt$labels
+
+# # Endothelial cell-related markers
+# library(scater)
+# plotHeatmap(scRNA, order_columns_by="labels",
+#             features = unique(unlist(all.markers[["Endothelial_cells"]])))
 
 
 
-##### Run singleR #####
-  #### Presetting ####
-  SingleRResult.lt <- Anno_SingleR(scRNA.SeuObj, RefType = RefType, celldexDatabase = celldexDatabase,
-                                   CTFeatures.SeuObj = CTFeatures.SeuObj,
-                                   quantile = quantile, tune.thresh = tune.thresh, sd.thresh = sd.thresh,
-                                   de.method = de.method,
-                                   Remark = Remark,
-                                   RefName = RefName,
-                                   RefName2 = RefName2,
-                                   Save.Path = paste0(Save.Path,"/",Remark), ProjectName = "CT")
-  scRNA.SeuObj <- SingleRResult.lt[["scRNA.SeuObj"]]
+pdf(file = paste0(Save.Path,"/",ProjectName,"_",Remark,"_HeatmapCTmarkers.pdf"),
+    width = 12,  height = 7
+)
+for (i in 1:length(all.markers)) {
+  plotHeatmap(scRNA, order_columns_by = paste0("labels_",SingleR_DE_method,"_",Remark),
+              features=unique(unlist(all.markers[[i]]))) %>% print()
+}
+dev.off()
 
-#   #### Try Parameter ####
-#   CC_Anno.df <- as.data.frame(matrix(nrow=0, ncol=7))
-#   colnames(CC_Anno.df) <- c("TestID", "Tool", "Type","Set", "quantile", "tune_Thr","SD_Thr")
+
+
+
+## Plot UMAP
+scRNA.SeuObj@meta.data[[paste0("singleR_",SingleR_DE_method,"_",Remark)]]<- SingleR.lt$labels # scRNA.SeuObj$singleRPredbyCTDB <- SingleR.lt$labels
+p.CTPred1 <- DimPlot(scRNA.SeuObj, reduction = "umap", group.by = paste0("singleR_",SingleR_DE_method,"_",Remark) ,label = TRUE, pt.size = 0.5) + NoLegend()
+p.CTPred1
+p.CT1 <- DimPlot(scRNA.SeuObj, reduction = "umap", group.by ="Cell_type" ,label = TRUE, pt.size = 0.5) + NoLegend()
+p.CT1
+
+library(ggpubr)
+p.CTComp1 <- ggarrange(p.CT1, p.CTPred1, common.legend = TRUE, legend = "top")
+p.CTComp1
+
+pdf(file = paste0(Save.Path,"/",ProjectName,"_",Remark,"_CompareCTUMAP.pdf"),
+    width = 12,  height = 7
+)
+p.CTComp1 %>% print()
+dev.off()
+
+
+
+pdf(file = paste0(Save.Path,"/",ProjectName,"_",Remark,"_CompareCTUMAP.pdf"),
+    width = 12,  height = 7
+)
+p.CTPred1 %>% BeautifyggPlot(XaThick=1,  YaThick=1, LegPos = c(1, 0.5)) %>%
+  print()
+## Change color
+library(ggsci)
+p.CTPred1 + scale_color_npg() + coord_fixed(ratio = 1) + theme(legend.position = c(1, 0.5))
+
+dev.off()
+
+
+
+
+##### Session information #####
+sessionInfo()
+## Ref: https://stackoverflow.com/questions/21967254/how-to-write-a-reader-friendly-sessioninfo-to-text-file
+writeLines(capture.output(sessionInfo()), paste0(Save.Path,"/sessionInfo.txt"))
+
+
+
+##### Save RData #####
+# save.image(paste0("D:/Dropbox/#_Dataset/Cancer/PDAC/",Sys.Date(),"_SeuratObject_",ProjectName,".RData"))
+save.image(paste0("D:/Dropbox/#_Dataset/Cancer/PDAC/",Version,".RData"))
+
+
+##### ************************************************************************ #####
+scRNA.SeuObj@meta.data$singleR_classic_PredbyscRNA2 <- scRNA.SeuObj@meta.data$singleR_classic_PredbyscRNA  %>% as.character()
+TTT1 <- scRNA.SeuObj@meta.data
+TTT_Ref <- scRNA.SeuObj_Ref@meta.data
+TTT1[TTT1$CELL %in% TTT_Ref$CELL,]$singleR_classic_PredbyscRNA2 <- TTT_Ref$celltype %>% as.character()
+scRNA.SeuObj@meta.data <- TTT1
+
+DimPlot(scRNA.SeuObj, reduction = "umap",group.by = "singleR_classic_PredbyscRNA2")
+DimPlot(scRNA.SeuObj, reduction = "umap",group.by = "singleR_classic_PredbyscRNA")
+DimPlot(scRNA.SeuObj, reduction = "umap",group.by = "celltype")
+DimPlot(scRNA.SeuObj, reduction = "umap",group.by = "Type")
+
+##### ************************************************************************ #####
+
+
+# ## 20221113 ##
+# ## ************************************************************************************************************************ ##
 #
-#   #### PredbyCTDB ####
-#   for (i in seq(0.6,1,0.2)) {
-#     for (j in seq(0.03,0.07,0.01)) {
-#       for (k in c(1,2)) {
-#         Remark1 <- "PredbyCTDB"
-#         de.method <- "classic"
-#         RefType <- "BuiltIn_celldex"
-#         Remark <- paste0(Remark1,"_",de.method,"_",
-#                          "qua",i,"_tun",j,"_sd",k)
-#
-#         SingleRResult.lt <- Anno_SingleR(scRNA.SeuObj, RefType = RefType, celldexDatabase = "HumanPrimaryCellAtlasData",
-#                                          quantile = i, tune.thresh = j, sd.thresh = k,
-#                                          Remark = Remark,Save.Path = paste0(Save.Path,"/",Remark), ProjectName = "CT")
-#         scRNA.SeuObj <- SingleRResult.lt[["scRNA.SeuObj"]]
-#
-#         CC_Anno_Temp.df <- data.frame(TestID = "Predict", Tool = "singleR", Type = "PDAC",
-#                                       Set = Remark1, quantile = i, tune_Thr = j, SD_Thr = k)
-#         CC_Anno.df <- rbind(CC_Anno.df, CC_Anno_Temp.df)
-#       }
-#     }
-#   }
-#   rm(i,j,k,CC_Anno_Temp.df, Remark, Remark1, de.method, RefType)
-#
-#     #### Create check dataframe ####
-#     CC.df <- scRNA.SeuObj@meta.data[,(ncol(scRNA.SeuObj@meta.data)-nrow(CC_Anno.df)+1):ncol(scRNA.SeuObj@meta.data)]
-#     CC_Anno.df$TestID <- colnames(CC.df)
-#
-#     # TTT <- gsub(CC.df, pattern = " ", replacement = "_")
-#     # TTT <- sub(" ", "_", CC.df)
-#     # TTT <- CC.df
-#     CC.df <- lapply(CC.df, gsub, pattern = "_", replacement = " ", fixed = TRUE) %>%
-#              lapply(., gsub, pattern = "cells", replacement = "cell", fixed = TRUE) %>%
-#              lapply(., gsub, pattern = "Macrophage", replacement = "Macrophage cell", fixed = TRUE) %>%
-#              lapply(., gsub, pattern = "Fibroblasts", replacement = "Fibroblast cell", fixed = TRUE) %>%
-#              lapply(., gsub, pattern = "Epithelial cell", replacement = "Ductal cell type 1", fixed = TRUE) %>%
-#              as.data.frame()
-#
-#     CC_CT.df <- data.frame(Cell_type = scRNA.SeuObj@meta.data[,"Cell_type"])
-#     CC.df <- cbind(CC_CT.df, CC.df)
-#     rm(CC_CT.df)
-#
-#     CTReplace <- function(CC.df,ColN=1, ReferCT = "Actual",Replacement="Other") {
-#       CC.df[!CC.df[,ColN] %in% c(CC.df[,ReferCT] %>% unique()),][,ColN] <- Replacement
-#       return(CC.df)
-#     }
-#
-#     for (i in 2:ncol(CC.df)) {
-#       CC.df <- CTReplace(CC.df,ColN=i, ReferCT = "Cell_type",Replacement="Other")
-#     }
-#
-#
-#   #### PredbyscRNA ####
-#   CC_Anno2.df <- as.data.frame(matrix(nrow=0, ncol=7))
-#   colnames(CC_Anno2.df) <- c("TestID", "Tool", "Type","Set", "quantile", "tune_Thr","SD_Thr")
-#
-#   for (i in seq(0.6,1,0.2)) {
-#     for (j in seq(0.03,0.07,0.01)) {
-#       for (k in c(1,2)) {
-#         Remark1 <- "PredbyscRNA"
-#         de.method <- "classic"
-#         RefType <- "BuiltIn_scRNA"
-#         Remark <- paste0(Remark1,"_",de.method,"_",
-#                          "qua",i,"_tun",j,"_sd",k)
-#
-#         SingleRResult.lt <- Anno_SingleR(scRNA.SeuObj, RefType = RefType, celldexDatabase = "HumanPrimaryCellAtlasData",
-#                                          quantile = i, tune.thresh = j, sd.thresh = k,CTFeatures.SeuObj = CTFeatures.SeuObj,
-#                                          Remark = Remark, Save.Path = paste0(Save.Path,"/",Remark), ProjectName = "CT")
-#         scRNA.SeuObj <- SingleRResult.lt[["scRNA.SeuObj"]]
-#
-#         CC_Anno_Temp.df <- data.frame(TestID = "Predict", Tool = "singleR", Type = "PDAC",
-#                                       Set = Remark1, quantile = i, tune_Thr = j, SD_Thr = k)
-#         CC_Anno2.df <- rbind(CC_Anno2.df, CC_Anno_Temp.df)
-#       }
-#     }
-#   }
-#   rm(i,j,k,CC_Anno_Temp.df, Remark, Remark1, de.method, RefType)
-#
-#     #### Create check dataframe ####
-#     CC2.df <- scRNA.SeuObj@meta.data[,(ncol(scRNA.SeuObj@meta.data)-nrow(CC_Anno2.df)+1):ncol(scRNA.SeuObj@meta.data)]
-#     CC_Anno2.df$TestID <- colnames(CC2.df)
-#     CC_Anno.df <- rbind(CC_Anno.df,CC_Anno2.df)
-#
-#     CC_CT.df <- data.frame(Cell_type = scRNA.SeuObj@meta.data[,"Cell_type"])
-#     CC.df <- cbind(CC.df, CC2.df)
-#     rm(CC_CT.df,CC2.df,CC_Anno2.df)
-#
-#     CC.df$Cell_type <- as.character(CC.df$Cell_type)
-#     CC.df <- rbind(CC.df,"Other")
 #
 # ##### Verification (CellCheck) #####
 #   #### Install ####
@@ -211,36 +286,46 @@ if(SmallTest == TRUE){
 #   library(CellCheck)
 #
 #   #### Run CellCheck ####
+#   ## Create check dataframe
+#   CC.df <- scRNA.SeuObj@meta.data[,c("Cell_type","singleR_classic_PredbyscRNA", "singleR_classic_PredbyCTDB")]
+#
+#   CC.df <- data.frame(lapply(CC.df, as.character), stringsAsFactors=FALSE)
+#
+#   colnames(CC.df) <- c("Actual","Predict1","Predict2")
+#   #CC.df$Actual <- as.character(CC.df$Actual)
+#
+#   CC.df$Predict2 <- gsub("_", " ", CC.df$Predict2)
+#   CC.df$Predict2 <- gsub("cells", "cell", CC.df$Predict2)
+#   CC.df$Predict2 <- gsub("Macrophage", "Macrophage cell", CC.df$Predict2)
+#   CC.df$Predict2 <- gsub("Fibroblasts", "Fibroblast cell", CC.df$Predict2)
+#   CC.df$Predict2 <- gsub("Epithelial cell", "Ductal cell type 1", CC.df$Predict2)
+#
+#
+#
+#   CC.df[!CC.df$Predict2 %in% c(CC.df$Actual %>% unique()),]$Predict2 <- "Other"
+#   # CC.df <- rbind(CC.df,"NotMatch")  #CC.df[nrow(CC.df)+1,1:ncol(CC.df)] <- "Other"
+#
+#   CC_Anno.df <- data.frame(TestID = c("Predict1","Predict2"),
+#                            Tool = "singleR",
+#                            Type = "PDAC",
+#                            Set = c("singleRPredbyscRNA", "singleRPredbyCTDB"))
 #
 #   ## For one prediction
-#   DisCMSet.lt = list(Mode = "One", Actual = "Cell_type", Predict = "singleR_PredbyCTDB_classic_qua0.6_tun0.03_sd1" , FilterSet1 = "Tool", FilterSet2 = "singleR" , Remark = "") # Mode = c("One","Multiple")
+#   ## For one prediction
+#   DisCMSet.lt = list(Mode = "One", Actual = "Actual", Predict = "Predict1" , FilterSet1 = "Tool", FilterSet2 = "singleR" , Remark = "") # Mode = c("One","Multiple")
 #   BarChartSet.lt <- list(Mode = "One", Metrics = "Balanced.Accuracy", XValue = "Set", Group = "Tool", Remark = "")
-#   LinePlotSet.lt <- list(Mode = "One", Metrics = "Balanced.Accuracy", XValue = "tune_Thr", Group = "Set", Remark = "")
+#   LinePlotSet.lt <- list(Mode = "One", Metrics = "Balanced.Accuracy", XValue = "Set", Group = "Tool", Remark = "")
 #   CCR_cm_DisMult.lt <- CellCheck_DisMult(CC.df, CC_Anno.df,
 #                                          DisCMSet.lt = DisCMSet.lt,
 #                                          BarChartSet.lt = BarChartSet.lt,
 #                                          LinePlotSet.lt = LinePlotSet.lt,
-#                                          Save.Path = Save.Path, ProjectName = paste0("CellCheck_",ProjectName_Ano))
-#
-#   ## For multiple prediction
-#   DisCMSet.lt = list(Mode = "Multiple", Actual = "Cell_type", FilterSet1 = "Tool", FilterSet2 = "singleR" , Remark = "_All") # Mode = c("One","Multiple")
-#   BarChartSet.lt <- list(Mode = "Multiple", XValue = "Set", Group = "Tool", Remark = "_All")
-#   LinePlotSet.lt <- list(Mode = "Multiple", XValue = "tune_Thr", Group = "Set", Remark = "_All")
-#   Sum_DisMult.df <- CellCheck_DisMult(CC.df, CC_Anno.df,
-#                                       DisCMSet.lt = DisCMSet.lt,
-#                                       BarChartSet.lt = BarChartSet.lt,
-#                                       LinePlotSet.lt=LinePlotSet.lt,
-#                                       Save.Path = Save.Path, ProjectName = paste0("CellCheck_",ProjectName_Ano))
+#                                          Save.Path = Save.Path, ProjectName = paste0("CellCheck_",ProjectName))
 #
 #
-
-# ##### Session information #####
-#   sessionInfo()
-#   ## Ref: https://stackoverflow.com/questions/21967254/how-to-write-a-reader-friendly-sessioninfo-to-text-file
-#   writeLines(capture.output(sessionInfo()), paste0(Save.Path,"/sessionInfo.txt"))
 #
 # ##### Save RData #####
-#   save.image(paste0(Save.Path,"/SeuratObject_",ProjectName_Ano,".RData"))
-
-
-
+#   save.image(paste0("D:/Dropbox/#_Dataset/Cancer/PDAC/",Sys.Date(),"_SeuratObject_",ProjectName,".RData"))
+#
+#
+#
+#
