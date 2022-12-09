7cd552afaf494d93abdb402fa2d6a909cfb9ac90
Charlene <q2330416@gmail.com>
2022-12-09 13:26:25 +0800

    Modify to scRNA_Sub.SeuObj

--- #_inferCNV.R
+++ #_inferCNV.R
@@ -9,21 +9,29 @@ library(tidyverse)
 
 ##### Load data ######
 ## Load all
-load("D:/Dropbox/#_Dataset/Cancer/PDAC/2022-12-05_CTAnno_singleR_RefPRJCA001063_PDAC.RData")
+# load("D:/Dropbox/#_Dataset/Cancer/PDAC/2022-12-05_CTAnno_singleR_RefPRJCA001063_PDAC.RData")
+load("D:/Dropbox/#_Dataset/Cancer/PDAC/2022-12-07_TrajAna_PCA_PDAC_ROGUE_Monocle3.RData")
+
 ## Clean up the object
-rm(list=setdiff(ls(), c("scRNA.SeuObj")))
+rm(list=setdiff(ls(), c("scRNA.SeuObj","scRNA_Sub.SeuObj")))
+
+## plot UMAP
+library(Seurat)
+scRNA_Sub.SeuObj@meta.data$Cell_type <- as.factor(scRNA_Sub.SeuObj@meta.data$Cell_type)
+scRNA_Sub.SeuObj@meta.data$Cell_type <- gsub("_", " ", scRNA_Sub.SeuObj@meta.data$Cell_type)
+scRNA_Sub.SeuObj <- scRNA_Sub.SeuObj[,!scRNA_Sub.SeuObj$Cell_type %in% c("T cell","Fibroblast cell")]
+
+DimPlot(scRNA_Sub.SeuObj, reduction = "umap",group.by = "Cell_type")
 
 
 ##### Current path and new folder setting* #####
-ProjectName = paste0("inferCNV")
+ProjectName = paste0("inferCNV_Sub")
 Sampletype = "PDAC"
 
 Version = paste0(Sys.Date(),"_",ProjectName,"_",Sampletype)
 Save.Path = paste0(getwd(),"/",Version)
 ## Create new folder
-if (!dir.exists(Save.Path)){
-  dir.create(Save.Path)
-}
+if (!dir.exists(Save.Path)){dir.create(Save.Path)}
 
 RData_Save.Path = "D:/Dropbox/#_Dataset/Cancer/PDAC/"
 
@@ -33,13 +41,11 @@ Species = "Human"
 ##### 013 inferCNV #####
 ## Create new folder
 PathinferCNV <- paste0(Save.Path,"/","D01_inferCNV")
-if (!dir.exists(PathinferCNV)){
-  dir.create(PathinferCNV)
-}
+if (!dir.exists(PathinferCNV)){dir.create(PathinferCNV)}
 
 source("FUN_inferCNV.R")
 
-infercnv_obj <- inferCNV(scRNA.SeuObj, AnnoSet = "singleR_classic_PredbyscRNA",
+infercnv_obj <- inferCNV(scRNA_Sub.SeuObj, AnnoSet = "Cell_type",
                          SpeciSet = Species,
                          Path = PathinferCNV,
                          RefSet = c("Acinar cell"),
