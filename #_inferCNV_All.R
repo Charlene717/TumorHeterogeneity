166b4331e5ef0064136cb567d89dc905aeecda37
Charlene <q2330416@gmail.com>
2022-12-14 09:15:08 +0800

    Change to Cell_type

--- #_inferCNV_All.R
+++ #_inferCNV_All.R
@@ -22,7 +22,7 @@ scRNA.SeuObj <- scRNA_Small.SeuObj
 
 scRNA.SeuObj@meta.data$Cell_type <- gsub(" ", "_", scRNA.SeuObj@meta.data$Cell_type) #Note!!# Will make factor become character!!
 scRNA.SeuObj@meta.data$Cell_type <- as.factor(scRNA.SeuObj@meta.data$Cell_type)
-
+scRNA.SeuObj <- scRNA.SeuObj[,!scRNA.SeuObj$Cell_type %in% c(NA)]
 
 
 ## plot UMAP
@@ -32,7 +32,7 @@ DimPlot(scRNA.SeuObj, reduction = "umap",group.by = "Cell_type")
 
 ##### Current path and new folder setting* #####
 ProjectName = paste0("inferCNV")
-Sampletype = "PDAC"
+Sampletype = "PDAC2"
 
 Version = paste0(Sys.Date(),"_",ProjectName,"_",Sampletype)
 Save.Path = paste0(getwd(),"/",Version)
@@ -55,10 +55,18 @@ if (!dir.exists(PathinferCNV)){
 
 source("FUN_inferCNV.R")
 
-infercnv_obj <- inferCNV(scRNA.SeuObj, AnnoSet = "singleR_classic_PredbyscRNA",
+# infercnv_obj <- inferCNV(scRNA.SeuObj, AnnoSet = "singleR_classic_PredbyscRNA",
+#                          SpeciSet = Species,
+#                          Path = PathinferCNV,
+#                          RefSet = c("Acinar cell"),
+#                          CreateInfercnvObject.lt = list(chr_exclude = c("chrM")))
+
+infercnv_obj <- inferCNV(scRNA.SeuObj, AnnoSet = "Cell_type",
                          SpeciSet = Species,
                          Path = PathinferCNV,
-                         RefSet = c("Acinar cell"),
-                         CreateInfercnvObject.lt = list(chr_exclude = c("chrM")))
+                         RefSet = c("Acinar_cell"),
+                         CreateInfercnvObject.lt = list(delim="\t",max_cells_per_group = NULL,min_max_counts_per_cell = c(100, +Inf),chr_exclude = c("chrX", "chrY", "chrM")))
+
+
 ##### save.image #####
 save.image(paste0(RData_Save.Path,"/",Version,".RData"))
